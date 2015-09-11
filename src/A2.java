18e20e98d1ced81770f7cfa1e719a4f3ede96ac8
unknown <x15g016@PC505N012.chiba-fjb.ac.jp>
2015-09-11 11:51:25 +0900

    ついかあああ

--- src/A2.java
+++ src/A2.java
@@ -4,6 +4,7 @@ public class A2 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("ああわあ");
+		System.out.println("ああわあ");System.out.println("ああわあ");System.out.println("ああわあ");
 	}
 
 }
